118538afce1143bb7517307ed035d9ccacfee6ce
Lawrence Surges <surgeslc@gmail.com>
2017-07-13 22:49:01 +0000

    Dynamic Tweets implemented

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,30 +4,90 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+  var tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
 function createTweetElement(tweetData) {
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  var tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-    "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-   },
-  "created_at": 1461116232227
-  }
-
-  var $tweet = $("<article>").addClass("tweet");
-
-  return $tweet;
+/*  var $avatar = $('<img src="https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png" alt="User avatar">');
+  var $userName = "Bill Fields";
+  $tweet.append($avatar);
+*/
+  return $('<article class="tweet">')
+    .append($('<header>')
+      .append($('<div class="user">')
+        .append($('<img>').attr('src', tweetData.user.avatars.small).attr('alt', 'User avatar'))
+        .append($('<span class="userName">').text(tweetData.user.name))
+        .append($('<span class="userHandle">').text(tweetData.user.handle))
+      )
+    )
+    .append($('<main>')
+      .append($('<p>').text(tweetData.content.text))
+    )
+    .append($('<footer>')
+      .append($('<span>').attr('data-livestamp', (tweetData.created_at / 1000)))
+      .append($('<div class="tweeterButtons">')
+        .append($('<abbr class="fa fa-flag" title="Flag">'))
+        .append($('<abbr class="fa fa-retweet" title="Retweet">'))
+        .append($('<abbr class="fa fa-heart" title="Like">'))
+      )
+    );
 }
-// Test / driver code (temporary)
-console.log($tweet); // see what it looks like
-$('#tweets-container').append($tweet); /* add it to the page to ensure it's got
-all the right elements, classes, etc.*/
+
+function renderTweets(tweets) {
+
+//$('#tweets-container').append($tweetElements);
+  tweets.forEach(function(tweet) {
+    let $tweet = createTweetElement(tweet);
+    $('#tweets-container').append($tweet);
+  });
+}
+
+$(document).ready(function() {
+  var $tweetElements = renderTweets(tweetData);
+});
