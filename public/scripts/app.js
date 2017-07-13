172b9f2cd1fd63f4f067103836affc513912bcaa
Lawrence Surges <surgeslc@gmail.com>
2017-07-13 20:25:55 +0000

    Header & footer inside Article

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+function createTweetElement(tweetData) {
+
+  // Test / driver code (temporary). Eventually will get this from the server.
+  var tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+    "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+   },
+  "created_at": 1461116232227
+  }
+
+  var $tweet = $("<article>").addClass("tweet");
+
+  return $tweet;
+}
+// Test / driver code (temporary)
+console.log($tweet); // see what it looks like
+$('#tweets-container').append($tweet); /* add it to the page to ensure it's got
+all the right elements, classes, etc.*/
