19e20b156091c4d5db43734f01654ecd7261fdb2
Lawrence Surges <surgeslc@gmail.com>
2017-07-14 03:34:25 +0000

    mongodb accessing data

--- server/index.js
+++ server/index.js
@@ -11,24 +11,66 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+//LCS: Next line to be commented out, replaced by a handle to mongodb
+//const db = require("./lib/in-memory-db");
 
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
+//Code copied from mongo_example.js
+const {MongoClient} = require("mongodb");
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+//Added to minimize changes to legacy code
 
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
-// Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
+var mongodb = MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
 
-app.listen(PORT, () => {
-  console.log("Tweetr app listening on port " + PORT);
+  // We have a connection to the "tweeter" db, starting here.
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+  // ==> Refactored and wrapped as new, tweet-specific function:
+  function getTweets(callback) {
+    db.collection("tweets").find().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, tweets);
+    });
+  }
+
+  // ==> Later it can be invoked. Remember even if you pass `getTweets` to another scope,
+  // it still has closure over `db`, so it will still work. Yay!
+
+  getTweets((err, tweets) => {
+    if (err) throw err;
+
+  /*  console.log("Logging each tweet:");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    } */
+
+    //db.close();
+  });
+
+  // The `data-helpers` module provides an interface to the database of tweets.
+  // This simple interface layer has a big benefit: we could switch out the
+  // actual database it uses and see little to no changes elsewhere in the code
+  // (hint hint).
+  //
+  // Because it exports a function that expects the `db` as a parameter, we can
+  // require it and pass the `db` parameter immediately:
+  const DataHelpers = require("./lib/data-helpers.js")(db);
+  //const DataHelpers = require(mongodb)(db);
+
+  // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
+  // so it can define routes that use it to interact with the data layer.
+  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+
+  // Mount the tweets routes at the "/tweets" path prefix:
+  app.use("/tweets", tweetsRoutes);
+
+  app.listen(PORT, () => {
+    console.log("Tweetr app listening on port " + PORT);
+  });
 });
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
+//const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
@@ -9,19 +9,22 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
+      /*simulateDelay(() => {
         db.tweets.push(newTweet);
         callback(null, true);
-      });
+      });*/
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      db.collection('tweets').find().toArray(function stepTwo(queryErr, results) {
+        if(queryErr) {
+          console.log('Query Error', queryErr);
+        } else {
+          console.log('Query Results', results);
+        }
+        db.close();
       });
     }
-
   };
 }
